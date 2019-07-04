1eea263e105d8a70e792f29f481cc8fd1686c85f
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-07-04 11:25:12 -0300

    A better view

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const initialState = [
     [undefined,undefined,1,1,1,undefined,undefined],
     [undefined,undefined,1,1,1,undefined,undefined],
     [1,1,1,1,1,1,1],
-    [1,1,1,0,1,1,1],
+    [1,1,1,undefined,1,1,1],
     [1,1,1,1,1,1,1],
     [undefined,undefined,1,1,1,undefined,undefined],
     [undefined,undefined,1,1,1,undefined,undefined],
 ];
 
-let dynamicBoard = '<ul>';
+let dynamicBoard = '<ul id = "lista">';
 
 for(let  i=0; i < initialState.length;  i++){
     dynamicBoard += '<li>';
     for(let j=0; j < initialState[i].length; j++){
         if(initialState[i][j] == 1){
-            dynamicBoard += '<button></button>';
+            dynamicBoard += '<button class = "defined"></button>';
         }
+        if(initialState[i][j] == undefined){
+            dynamicBoard += '<button class = "undefined"></button>';
+        }
+        
         
     }
     dynamicBoard += '</li>';
