6af0ecf902c201df57989858382483ec9a71ece4
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-07-04 10:50:11 -0300

    Initial Values

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const initialState = [
     [undefined,undefined,1,1,1,undefined,undefined],
     [undefined,undefined,1,1,1,undefined,undefined],
 ];
+
+for(let  i=0; i < initialState.length;  i++){
+    console.log('ROW')
+    for(let j=0; j < initialState[i].length; j++){
+        console.log(initialState[i][j])
+    }
+}
