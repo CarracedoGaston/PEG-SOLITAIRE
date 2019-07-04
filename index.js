576d1478512677be1b0a144d4c101e851fb66df4
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-07-04 11:09:04 -0300

    Structure with buttons

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const initialState = [
     [undefined,undefined,1,1,1,undefined,undefined],
 ];
 
+let dynamicBoard = '<ul>';
+
 for(let  i=0; i < initialState.length;  i++){
-    console.log('ROW')
+    dynamicBoard += '<li>';
     for(let j=0; j < initialState[i].length; j++){
-        console.log(initialState[i][j])
+        if(initialState[i][j] == 1){
+            dynamicBoard += '<button></button>';
+        }
+        
     }
+    dynamicBoard += '</li>';
+}
+dynamicBoard += '</ul>';
+
+window.onload = function(){
+// Obtenemos el elemento dibujado en el HTML
+const boardElement = document.getElementById('board')
+
+// Asignamos dynamicboard
+boardElement.innerHTML = dynamicBoard 
 }
